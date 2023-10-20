86f0d290a51c25ea63de75b1cc4f37e33a0e8ea0
Mako <759506695@qq.com>
2023-10-20 15:23:39 +0800

    sd

--- mae_pretrain.py
+++ mae_pretrain.py
@@ -6,6 +6,7 @@ import torchvision
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms import ToTensor, Compose, Normalize
 from tqdm import tqdm
+import myKit
 
 from model import *
 from utils import setup_seed
@@ -32,8 +33,13 @@ if __name__ == '__main__':
     assert batch_size % load_batch_size == 0
     steps_per_update = batch_size // load_batch_size
 
+    bone_dir = "../archive"
+    csv_name = "boneage-training-dataset.csv"
+    train_df, valid_df = myKit.split_data(bone_dir, csv_name, 20, 0.1, 128)
+    train_dataset, val_dataset = myKit.create_data_loader(train_df, valid_df)
+
     train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
-    val_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
+    # val_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
     dataloader = torch.utils.data.DataLoader(train_dataset, load_batch_size, shuffle=True, num_workers=4)
     writer = SummaryWriter(os.path.join('logs', 'cifar10', 'mae-pretrain'))
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
