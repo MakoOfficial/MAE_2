4da901da7214fbdaa017157574695d346d8b3e2e
Mako <759506695@qq.com>
2023-11-03 15:16:58 +0800

    init for dogClass

--- mae_pretrain.py
+++ mae_pretrain.py
@@ -6,22 +6,24 @@ import torchvision
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms import ToTensor, Compose, Normalize
 from tqdm import tqdm
-import myKit
-
+import torchvision.transforms as transforms
 from model import *
 from utils import setup_seed
+import torchvision.datasets as datasets
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--batch_size', type=int, default=4096)
-    parser.add_argument('--max_device_batch_size', type=int, default=512)
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--max_device_batch_size', type=int, default=64)
     parser.add_argument('--base_learning_rate', type=float, default=1.5e-4)
     parser.add_argument('--weight_decay', type=float, default=0.05)
     parser.add_argument('--mask_ratio', type=float, default=0.75)
-    parser.add_argument('--total_epoch', type=int, default=2000)
-    parser.add_argument('--warmup_epoch', type=int, default=200)
+    parser.add_argument('--total_epoch', type=int, default=800)
+    parser.add_argument('--warmup_epoch', type=int, default=40)
     parser.add_argument('--model_path', type=str, default='vit-t-mae.pt')
+    parser.add_argument('--data_path', default='../dog-breed-identification/train_valid_test/', type=str,
+                        help='dataset path')
 
     args = parser.parse_args()
 
@@ -33,18 +35,21 @@ if __name__ == '__main__':
     assert batch_size % load_batch_size == 0
     steps_per_update = batch_size // load_batch_size
 
-    bone_dir = "../archive"
-    csv_name = "boneage-training-dataset.csv"
-    train_df, valid_df = myKit.split_data(bone_dir, csv_name, 20, 0.1, 128)
-    train_dataset, val_dataset = myKit.create_data_loader(train_df, valid_df)
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop(args.input_size, scale=(0.2, 1.0), interpolation=3),  # 3 is bicubic
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
     # train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
     # val_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
+    train_dataset = datasets.ImageFolder(os.path.join(args.data_path, 'train'), transform=transform_train)
+    val_dataset = datasets.ImageFolder(os.path.join(args.data_path, 'valid'), transform=transform_train)
     dataloader = torch.utils.data.DataLoader(train_dataset, load_batch_size, shuffle=True, num_workers=4)
     writer = SummaryWriter(os.path.join('logs', 'cifar10', 'mae-pretrain'))
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    model = MAE_ViT(224, 16, mask_ratio=args.mask_ratio).to(device)
+    model = MAE_ViT(224, 16, emb_dim=1024, mask_ratio=args.mask_ratio, encoder_head=16, decoder_head=16).to(device)
     optim = torch.optim.AdamW(model.parameters(), lr=args.base_learning_rate * args.batch_size / 256, betas=(0.9, 0.95),
                               weight_decay=args.weight_decay)
     lr_func = lambda epoch: min((epoch + 1) / (args.warmup_epoch + 1e-8),
@@ -58,10 +63,9 @@ if __name__ == '__main__':
         losses = []
         for img, label in tqdm(iter(dataloader)):
             step_count += 1
-            print(img > shape)
-            # img = img.to(device)
+            img = img.to(device)
             # img = torch.einsum('nhwc->nchw', img)
-            img = img.type(torch.FloatTensor).to(device)
+            # img = img.type(torch.FloatTensor).to(device)
             predicted_img, mask = model(img)
             loss = torch.mean((predicted_img - img) ** 2 * mask) / args.mask_ratio
             loss.backward()
@@ -78,9 +82,9 @@ if __name__ == '__main__':
         model.eval()
         with torch.no_grad():
             val_img = torch.stack([val_dataset[i][0] for i in range(16)])
-            # val_img = val_img.to(device)
+            val_img = val_img.to(device)
             # val_img = torch.einsum('nhwc->nchw', val_img)
-            val_img = val_img.type(torch.FloatTensor).to(device)
+            # val_img = val_img.type(torch.FloatTensor).to(device)
             predicted_val_img, mask = model(val_img)
             predicted_val_img = predicted_val_img * mask + val_img * (1 - mask)
             img = torch.cat([val_img * (1 - mask), predicted_val_img, val_img], dim=0)
